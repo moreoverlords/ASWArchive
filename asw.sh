00830c1d712248fca0f3610500855cb45cc4e705
vi <me@vikramverma.com>
2015-06-05 07:44:21 +0800

    asw.sh: Skip the padding loop in favour of a printf.

--- asw.sh
+++ asw.sh
@@ -3,12 +3,7 @@
 for i in $(seq ${1:-1} ${2:-$(curl -s 'http://asofterworld.com/rssfeed.php' | xmllint --xpath "//item[1]/link/text()" - | grep -Po '\d+')}); do
     src="$(curl -s "http://www.asofterworld.com/index.php?id=$i" | xmllint --html --xpath '//div[@id="comicimg"]/img/@src' - 2>/dev/null | cut -d\" -f2)"
     name=`echo $src | cut -d"/" -f5`
-    number=""
-    for j in `seq 1 $(expr 4 - ${#i})`
-    do
-        number=0$number
-    done
-    number=$number$i
+    number="$(printf "%04d" $i)"
     echo $number
     name=$number$name
     echo NAME: $name
