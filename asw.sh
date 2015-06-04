f41f4fb7c10f003b506a0aa765546a3c899c01b0
vi <me@vikramverma.com>
2015-06-05 07:49:03 +0800

    asw.sh: wget output includes $number$name, so quit echoing.

--- asw.sh
+++ asw.sh
@@ -2,10 +2,6 @@
 
 for i in $(seq ${1:-1} ${2:-$(curl -s 'http://asofterworld.com/rssfeed.php' | xmllint --xpath "//item[1]/link/text()" - | grep -Po '\d+')}); do
     src="$(curl -s "http://www.asofterworld.com/index.php?id=$i" | xmllint --html --xpath '//div[@id="comicimg"]/img/@src' - 2>/dev/null | cut -d\" -f2)"
-    name=`echo $src | cut -d"/" -f5`
-    number="$(printf "%04d" $i)"
-    echo $number
-    name=$number$name
-    echo NAME: $name
+    name="$(printf "%04d$(cut -d/ -f5 <<<"$src")" $i)"
     wget -O $name $src
 done
