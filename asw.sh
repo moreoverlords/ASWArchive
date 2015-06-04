987e9fe2ff955ee410c48fdd694ef08dc77186b2
vi <me@vikramverma.com>
2015-06-05 07:34:26 +0800

    asw.sh: Grab the markup and "src" in one fell swoop.

--- asw.sh
+++ asw.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-for i in $(seq ${1:-1} ${2:-$(curl -s 'http://asofterworld.com/rssfeed.php' | xmllint --xpath "//item[1]/link/text()" - | grep -Po '\d+')})
-do
-    html=`curl "http://www.asofterworld.com/index.php?id=$i"`;
-    src=`echo $html | sed -n -e 's/.*img width=720 src="//p' | cut -d"\"" -f1`
+for i in $(seq ${1:-1} ${2:-$(curl -s 'http://asofterworld.com/rssfeed.php' | xmllint --xpath "//item[1]/link/text()" - | grep -Po '\d+')}); do
+    src="$(curl -s "http://www.asofterworld.com/index.php?id=$i" | xmllint --html --xpath '//div[@id="comicimg"]/img/@src' - 2>/dev/null | cut -d\" -f2)"
     name=`echo $src | cut -d"/" -f5`
     number=""
     for j in `seq 1 $(expr 4 - ${#i})`
